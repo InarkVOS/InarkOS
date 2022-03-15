6bcff6ade89c7a9e5b8640dee3310eb44fc6fb09
RevolvingMadness <andyallen216@gmail.com>
2022-03-15 09:11:43 -0500

    .

--- bin/help.py
+++ bin/help.py
@@ -1,5 +1,5 @@
 from colorama import Fore
 
 def help():
-        print('Commands: "animate", "clear", "clock", "echo", "exec", "shutdown", "pkgm-gui", "help", "ls", "pkgm"')
+        print('Commands: "matrix", "animate", "clear", "clock", "echo", "exec", "shutdown", "pkgm-gui", "help", "ls", "pkgm"')
         print(f'On detailed usage for commands please use the command {Fore.CYAN}tutr read {Fore.YELLOW}[command]{Fore.WHITE}')

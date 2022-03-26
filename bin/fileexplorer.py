dea455d6f9fa20e12049c4cef02a693329b5e505
RevolvingMadness <andyallen216@gamail.com>
2022-03-26 11:35:24 -0500

    Added Linux Copatibility

--- bin/fileexplorer.py
+++ bin/fileexplorer.py
@@ -8,6 +8,8 @@ else:
 			for e in input_generator:
 				if e == '\n':
 					return 'ENTER'
+				elif e == ' ':
+					return 'SPACE'
 				else:
 					return e.replace('KEY_', '')
 import sys
@@ -41,91 +43,129 @@ def update(path):
 	return len(files), files
 
 if '--run' in sys.argv:
-	try:
-		usr = sys.argv[sys.argv.index('--user')+1]
-		path = 'MainDrive/'
-		if os.name == 'nt':
-			while not done:
-				length, items = update(paths[-1])
-				key = ord(getch())
-				if key == 224:
-					keyc = ord(getch())
-					if keyc == 80:
-						if selidx != length-1:
-							selidx += 1
-					if keyc == 72:
-						if selidx != 0:
-							selidx -= 1
-				if key == 13 and items[selidx] == '>quit<':
-					os.system('cls')
-					done = 1
-				if key == 32 and items[selidx] not in customs:
-					if items[selidx] in selected:selected.remove(items[selidx])
-					else:selected.append(items[selidx])
-				elif key == 13:
-					if items[selidx] not in customs and os.path.isdir(paths[-1] + '/' + items[selidx]):
-						paths.append(paths[-1] + '/' + items[selidx])
-						selidx = 0
-					if items[selidx] == '>back<':
-						del paths[-1]
-						selidx = 0
-					if items[selidx] not in customs and os.path.isfile(paths[-1] + '/' + items[selidx]):
-						os.system(f'python3 bin/texteditor.py --run --user {usr} --file ' + paths[-1] + '/' + items[selidx])
-					if items[selidx] == '>delete<':
-						for i in range(len(items)):
-							if items[i] in selected:
-								idx = selected.index(items[i])
-								if os.name == 'nt':
-									thing = paths[-1].replace('/', '\\')
-									if os.path.isdir(f'{thing}\\{selected[idx]}'):
-										os.system(f'rmdir {thing}\\{selected[idx]}')
-									else:
-										os.system(f'del /f {thing}\\{selected[idx]}')
-								selected.remove(items[i])
-						selidx = 0
-					if len(selected) == 1 and '>rename<' in items and items[selidx] == '>rename<':
-						newname = input(f'Enter the new file name for {selected[0]}: ')
-						thing = os.getcwd().replace('\\', '/')
-						thinga = paths[-1].replace('\\', '/')
-						os.chdir(paths[-1])
-						os.system(f'rename {selected[0]} {newname}')
-						for i in range(len(paths[-1].split('/'))):
-							os.chdir('..')
-						selected.remove(selected[0])
-						selidx = 0
-					if items[selidx] == '>make file<':
-						os.chdir(paths[-1])
-						fname = input('Enter the file name: ')
-						open(fname, 'w').write('')
-						for i in range(len(paths[-1].split('/'))):
-							os.chdir('..')
-					if items[selidx] == '>make folder<':
-						os.chdir(paths[-1])
-						fname = input('Enter the folder name: ')
-						os.mkdir(fname)
-						for i in range(len(paths[-1].split('/'))):
-							os.chdir('..')
-		else:
-			while not done:
-				length, items = update(paths[-1])
-				key = getch()
-				if key == 'DOWN':
+	usr = sys.argv[sys.argv.index('--user')+1]
+	path = 'MainDrive/'
+	if os.name == 'nt':
+		while not done:
+			length, items = update(paths[-1])
+			key = ord(getch())
+			if key == 224:
+				keyc = ord(getch())
+				if keyc == 80:
 					if selidx != length-1:
 						selidx += 1
-				if key == 'UP':
+				if keyc == 72:
 					if selidx != 0:
 						selidx -= 1
-				if key == 'ENTER':
-					if items[selidx] == '>quit<':
-						os.system('clear')
-						done = 1
-					if items[selidx] != '>back<' and os.path.isdir(paths[-1] + '/' + items[selidx]):
-						paths.append(paths[-1] + '/' + items[selidx])
-						selidx = 0
-					if items[selidx] == '>back<':
-						del paths[-1]
-						selidx = 0
-					if items[selidx] != '>back<' and os.path.isfile(paths[-1] + '/' + items[selidx]):
-						os.system(f'python3 bin/texteditor.py --run --user {usr} --file ' + paths[-1] + '/' + items[selidx])
-	except:
-		pass
+			if key == 13 and items[selidx] == '>quit<':
+				os.system('cls')
+				done = 1
+			if key == 32 and items[selidx] not in customs:
+				if items[selidx] in selected:selected.remove(items[selidx])
+				else:selected.append(items[selidx])
+			elif key == 13:
+				if items[selidx] not in customs and os.path.isdir(paths[-1] + '/' + items[selidx]):
+					paths.append(paths[-1] + '/' + items[selidx])
+					selidx = 0
+				if items[selidx] == '>back<':
+					del paths[-1]
+					selidx = 0
+				if items[selidx] not in customs and os.path.isfile(paths[-1] + '/' + items[selidx]):
+					os.system(f'python3 bin/texteditor.py --run --user {usr} --file ' + paths[-1] + '/' + items[selidx])
+				if items[selidx] == '>delete<':
+					for i in range(len(items)):
+						if items[i] in selected:
+							idx = selected.index(items[i])
+							thing = paths[-1].replace('/', '\\')
+							if os.path.isdir(f'{thing}\\{selected[idx]}'):
+								os.system(f'rmdir {thing}\\{selected[idx]}')
+							else:
+								os.system(f'del /f {thing}\\{selected[idx]}')
+							selected.remove(items[i])
+					selidx = 0
+				if len(selected) == 1 and '>rename<' in items and items[selidx] == '>rename<':
+					newname = input(f'Enter the new file name for {selected[0]}: ')
+					thing = os.getcwd().replace('\\', '/')
+					thinga = paths[-1].replace('\\', '/')
+					os.chdir(paths[-1])
+					os.system(f'rename {selected[0]} {newname}')
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+					selected.remove(selected[0])
+					selidx = 0
+				if items[selidx] == '>make file<':
+					os.chdir(paths[-1])
+					fname = input('Enter the file name: ')
+					open(fname, 'w').write('')
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+				if items[selidx] == '>make folder<':
+					os.chdir(paths[-1])
+					fname = input('Enter the folder name: ')
+					os.mkdir(fname)
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+	else:
+		while not done:
+			length, items = update(paths[-1])
+			key = getch()
+			if key == 'DOWN':
+				if selidx != length-1:
+					selidx += 1
+			if key == 'UP':
+				if selidx != 0:
+					selidx -= 1
+			if key == 'ENTER':
+				if items[selidx] == '>quit<':
+					os.system('clear')
+					done = 1
+				if items[selidx] != '>back<' and os.path.isdir(paths[-1] + '/' + items[selidx]):
+					paths.append(paths[-1] + '/' + items[selidx])
+					selidx = 0
+				if items[selidx] == '>back<':
+					del paths[-1]
+					selidx = 0
+				if items[selidx] != '>back<' and os.path.isfile(paths[-1] + '/' + items[selidx]):
+					os.system(f'python3 bin/texteditor.py --run --user {usr} --file ' + paths[-1] + '/' + items[selidx])
+				if items[selidx] == '>make file<':
+					os.chdir(paths[-1])
+					fname = input('Enter the file name: ')
+					open(fname, 'w').write('')
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+				if items[selidx] == '>make folder<':
+					os.chdir(paths[-1])
+					fname = input('Enter the folder name: ')
+					os.mkdir(fname)
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+				if items[selidx] == '>delete<':
+					for i in range(len(items)):
+						if items[i] in selected:
+							idx = selected.index(items[i])
+							os.chdir(paths[-1])
+							if os.path.isdir(f'{selected[idx]}'):
+								os.system(f'rmdir {selected[idx]}')
+							else:
+								os.system(f'rm -f {selected[idx]}')
+							selected.remove(items[i])
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+					selidx = 0
+				if items[selidx] == 'rename':
+					newname = input(f'Enter the new file name for {selected[0]}: ')
+					thing = os.getcwd().replace('\\', '/')
+					thinga = paths[-1].replace('\\', '/')
+					os.chdir(paths[-1])
+					os.system(f'rename {selected[0]} {newname}')
+					for i in range(len(paths[-1].split('/'))):
+						os.chdir('..')
+					selected.remove(selected[0])
+					selidx = 0
+			if key == 'SPACE' and items[selidx] not in customs:
+				if items[selidx] in selected:selected.remove(items[selidx])
+				else:selected.append(items[selidx])
+
+
+	# except:
+		# pass
